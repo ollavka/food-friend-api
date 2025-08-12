b2522eb22a1a51ee549da449f7a695bc5328b584
Oleksii Lavka <oleksii.lavka.dev@gmail.com>
2025-08-12 21:54:31 +0300

    fix: fix auth swagger options

--- src/config/swagger/setup-swagger.config.ts
+++ src/config/swagger/setup-swagger.config.ts
@@ -7,10 +7,21 @@ export function setupSwagger(app: INestApplication): void {
     .setTitle('Food Friend API')
     .setDescription('API documentation for the Food Friend app')
     .setVersion('1.0.0')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+      },
+      'Access token',
+    )
+    .addSecurityRequirements('Access token')
     .build()
 
   SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, config, { extraModels: swaggerExtraModels }), {
     customSiteTitle: 'Food Friend API Docs',
+    swaggerOptions: { persistAuthorization: true },
   })
 }
