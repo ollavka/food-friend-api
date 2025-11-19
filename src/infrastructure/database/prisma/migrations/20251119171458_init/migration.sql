8e4027de40c6608bdd952bb54ef197fbaad4ec8c
Oleksii Lavka <oleksii.lavka.dev@gmail.com>
2025-11-19 19:15:22 +0200

    feat: update init db migration

--- src/infrastructure/database/prisma/migrations/20251119171458_init/migration.sql
+++ src/infrastructure/database/prisma/migrations/20251119171458_init/migration.sql
@@ -1,5 +1,11 @@
 -- CreateEnum
+CREATE TYPE "public"."AuthProvider" AS ENUM ('GOOGLE');
+
+-- CreateEnum
 CREATE TYPE "public"."OtpCodeType" AS ENUM ('EMAIL_VERIFICATION', 'PASSWORD_RESET');
+
+-- CreateEnum
+CREATE TYPE "public"."OtpCodeStatus" AS ENUM ('PENDING', 'CONSUMED', 'USED', 'EXPIRED');
 
 -- CreateEnum
 CREATE TYPE "public"."UserRole" AS ENUM ('REGULAR', 'ADMIN');
@@ -7,8 +13,18 @@ CREATE TYPE "public"."UserRole" AS ENUM ('REGULAR', 'ADMIN');
 -- CreateEnum
 CREATE TYPE "public"."UserStatus" AS ENUM ('UNVERIFIED', 'ACTIVE', 'BLOCKED');
 
--- CreateEnum
-CREATE TYPE "public"."AuthMethod" AS ENUM ('CREDENTIALS', 'GOOGLE');
+-- CreateTable
+CREATE TABLE "public"."accounts" (
+    "id" TEXT NOT NULL,
+    "provider" "public"."AuthProvider" NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "email" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "public"."otp_codes" (
@@ -17,6 +33,8 @@ CREATE TABLE "public"."otp_codes" (
     "email" TEXT NOT NULL,
     "code_hash" TEXT NOT NULL,
     "expires_at" TIMESTAMP(3) NOT NULL,
+    "status" "public"."OtpCodeStatus" NOT NULL DEFAULT 'PENDING',
+    "user_id" TEXT NOT NULL,
     "attempts" INTEGER NOT NULL DEFAULT 0,
     "window_started_at" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -29,6 +47,7 @@ CREATE TABLE "public"."otp_codes" (
 CREATE TABLE "public"."refresh_tokens" (
     "id" TEXT NOT NULL,
     "token_hash" TEXT NOT NULL,
+    "is_revoked" BOOLEAN NOT NULL DEFAULT false,
     "expires_at" TIMESTAMP(3) NOT NULL,
     "user_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -40,27 +59,34 @@ CREATE TABLE "public"."refresh_tokens" (
 -- CreateTable
 CREATE TABLE "public"."users" (
     "id" TEXT NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
     "email" TEXT NOT NULL,
     "password" TEXT,
     "role" "public"."UserRole" NOT NULL DEFAULT 'REGULAR',
     "status" "public"."UserStatus" NOT NULL DEFAULT 'UNVERIFIED',
-    "auth_method" "public"."AuthMethod" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "update_at" TIMESTAMP(3) NOT NULL,
+    "last_reset_password_mail_sent_at" TIMESTAMP(3),
+    "last_email_verification_mail_sent_at" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "accounts_user_id_idx" ON "public"."accounts"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "public"."accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_user_id_key" ON "public"."accounts"("provider", "user_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "otp_codes_code_hash_key" ON "public"."otp_codes"("code_hash");
 
 -- CreateIndex
 CREATE INDEX "otp_codes_email_type_idx" ON "public"."otp_codes"("email", "type");
-
--- CreateIndex
-CREATE INDEX "otp_codes_expires_at_idx" ON "public"."otp_codes"("expires_at");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "otp_codes_email_type_key" ON "public"."otp_codes"("email", "type");
@@ -79,6 +105,12 @@ CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
 
 -- CreateIndex
 CREATE INDEX "users_email_idx" ON "public"."users"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."otp_codes" ADD CONSTRAINT "otp_codes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
