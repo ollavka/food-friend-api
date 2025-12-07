3dbe0f597ce2f4a3ffab9e3b0a1d4776325b008a
Oleksii Lavka <oleksii.lavka.dev@gmail.com>
2025-12-08 01:02:17 +0200

    feat: update products seeds

--- src/infrastructure/database/prisma/seeds/product/products.data.ts
+++ src/infrastructure/database/prisma/seeds/product/products.data.ts
@@ -11,6 +11,46 @@ type ProductSeedData = {
 
 export const productsSeedData: ProductSeedData[] = [
   {
+    slug: 'sour-cream',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Sour cream' },
+      { languageCode: LanguageCode.UK, name: 'Сметана' },
+    ],
+  },
+  {
+    slug: 'egg',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Egg' },
+      { languageCode: LanguageCode.UK, name: 'Яйце' },
+    ],
+  },
+  {
+    slug: 'sugar',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Sugar' },
+      { languageCode: LanguageCode.UK, name: 'Цукор' },
+    ],
+  },
+  {
+    slug: 'flour',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Flour' },
+      { languageCode: LanguageCode.UK, name: 'Борошно' },
+    ],
+  },
+  {
+    slug: 'cottage-cheese',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Cottage cheese' },
+      { languageCode: LanguageCode.UK, name: 'Сир кисломолочний' },
+    ],
+  },
+  {
     slug: 'pork-ribs',
     measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
     translations: [
@@ -20,7 +60,7 @@ export const productsSeedData: ProductSeedData[] = [
   },
   {
     slug: 'beet',
-    measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
     translations: [
       { languageCode: LanguageCode.EN, name: 'Beet' },
       { languageCode: LanguageCode.UK, name: 'Буряк' },
@@ -43,6 +83,22 @@ export const productsSeedData: ProductSeedData[] = [
     ],
   },
   {
+    slug: 'carrot',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Carrot' },
+      { languageCode: LanguageCode.UK, name: 'Морква' },
+    ],
+  },
+  {
+    slug: 'onion',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Onion' },
+      { languageCode: LanguageCode.UK, name: 'Цибуля ріпчаста' },
+    ],
+  },
+  {
     slug: 'garlic',
     measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
     translations: [
@@ -51,43 +107,147 @@ export const productsSeedData: ProductSeedData[] = [
     ],
   },
   {
-    slug: 'sour-cream',
-    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    slug: 'tomato-paste',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
     translations: [
-      { languageCode: LanguageCode.EN, name: 'Sour cream' },
-      { languageCode: LanguageCode.UK, name: 'Сметана' },
+      { languageCode: LanguageCode.EN, name: 'Tomato paste' },
+      { languageCode: LanguageCode.UK, name: 'Томатна паста' },
     ],
   },
   {
-    slug: 'cottage-cheese',
+    slug: 'salmon-fillet',
     measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
     translations: [
-      { languageCode: LanguageCode.EN, name: 'Cottage cheese' },
-      { languageCode: LanguageCode.UK, name: 'Сир кисломолочний' },
+      { languageCode: LanguageCode.EN, name: 'Salmon fillet' },
+      { languageCode: LanguageCode.UK, name: 'Філе лосося' },
     ],
   },
   {
-    slug: 'egg',
+    slug: 'orange',
     measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
     translations: [
-      { languageCode: LanguageCode.EN, name: 'Egg' },
-      { languageCode: LanguageCode.UK, name: 'Яйце' },
+      { languageCode: LanguageCode.EN, name: 'Orange' },
+      { languageCode: LanguageCode.UK, name: 'Апельсин' },
     ],
   },
   {
-    slug: 'flour',
+    slug: 'honey',
     measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
     translations: [
-      { languageCode: LanguageCode.EN, name: 'Flour' },
-      { languageCode: LanguageCode.UK, name: 'Борошно' },
+      { languageCode: LanguageCode.EN, name: 'Honey' },
+      { languageCode: LanguageCode.UK, name: 'Мед' },
     ],
   },
   {
-    slug: 'sugar',
+    slug: 'soy-sauce',
     measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
     translations: [
-      { languageCode: LanguageCode.EN, name: 'Sugar' },
-      { languageCode: LanguageCode.UK, name: 'Цукор' },
+      { languageCode: LanguageCode.EN, name: 'Soy sauce' },
+      { languageCode: LanguageCode.UK, name: 'Соєвий соус' },
+    ],
+  },
+  {
+    slug: 'olive-oil',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Olive oil' },
+      { languageCode: LanguageCode.UK, name: 'Оливкова олія' },
+    ],
+  },
+  {
+    slug: 'chicken-fillet',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Chicken fillet' },
+      { languageCode: LanguageCode.UK, name: 'Куряче філе' },
+    ],
+  },
+  {
+    slug: 'romaine-lettuce',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Romaine lettuce' },
+      { languageCode: LanguageCode.UK, name: 'Салат Ромен' },
+    ],
+  },
+  {
+    slug: 'cherry-tomatoes',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Cherry tomatoes' },
+      { languageCode: LanguageCode.UK, name: 'Помідори чері' },
+    ],
+  },
+  {
+    slug: 'white-bread',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'White bread' },
+      { languageCode: LanguageCode.UK, name: 'Білий хліб' },
+    ],
+  },
+  {
+    slug: 'parmesan',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Parmesan' },
+      { languageCode: LanguageCode.UK, name: 'Пармезан' },
+    ],
+  },
+  {
+    slug: 'mustard',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Mustard' },
+      { languageCode: LanguageCode.UK, name: 'Гірчиця' },
+    ],
+  },
+  {
+    slug: 'lemon',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.COUNT,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Lemon' },
+      { languageCode: LanguageCode.UK, name: 'Лимон' },
+    ],
+  },
+  {
+    slug: 'pitted-cherry',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Pitted cherry' },
+      { languageCode: LanguageCode.UK, name: 'Вишня (без кісточок)' },
+    ],
+  },
+  {
+    slug: 'starch',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Starch' },
+      { languageCode: LanguageCode.UK, name: 'Крохмаль' },
+    ],
+  },
+  {
+    slug: 'spaghetti',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Spaghetti' },
+      { languageCode: LanguageCode.UK, name: 'Спагеті' },
+    ],
+  },
+  {
+    slug: 'bacon',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.MASS,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Bacon' },
+      { languageCode: LanguageCode.UK, name: 'Бекон' },
+    ],
+  },
+  {
+    slug: 'cream-20',
+    measurementBaseTypeKey: MeasurementBaseTypeKey.VOLUME,
+    translations: [
+      { languageCode: LanguageCode.EN, name: 'Cream 20%' },
+      { languageCode: LanguageCode.UK, name: 'Вершки 20%' },
     ],
   },
 ]
